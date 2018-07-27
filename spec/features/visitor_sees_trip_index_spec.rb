b116e32bd16fd19ba5550e37f6fb6874b85b43d4
JP Lynch <JPLynch35@gmail.com>
2018-07-27 10:22:54 -0600

    add test for trip show page, add trip show route

--- spec/features/visitor_sees_trip_index_spec.rb
+++ spec/features/visitor_sees_trip_index_spec.rb
@@ -11,5 +11,16 @@ describe 'a visitor' do
       expect(page).to have_content(trip1.name)
       expect(page).to have_content(trip2.name)
     end
+    it 'can click the name of the trip and see the trip show page' do
+      trip1 = Trip.create!(name: 'mytrip1', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 20:12'))
+      trip2 = Trip.create!(name: 'mytrip2', start_date: DateTime.parse('12/20/2015 12:12'), end_date: DateTime.parse('12/20/2015 12:12'))
+      visit trips_path
+
+      click_on trip1.name
+      expect(current_path).to eq(trip_path(trip1))
+      expect(page).to have_content(trip1.name)
+      expect(page).to have_content(trip1.start_date)
+      expect(page).to have_content(trip1.end_date)
+    end
   end
 end
