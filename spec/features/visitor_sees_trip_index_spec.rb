de8a0f1537ac72439adcc3e71317254da1247e6a
JP Lynch <JPLynch35@gmail.com>
2018-07-27 10:26:25 -0600

    add trip show page and made trip names links on trip index

--- spec/features/visitor_sees_trip_index_spec.rb
+++ spec/features/visitor_sees_trip_index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'a visitor' do
   describe "visits trips index page" do
     it 'sees a list of all trip names' do
-      trip1 = Trip.create!(name: 'mytrip1', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 12:12'))
-      trip2 = Trip.create!(name: 'mytrip2', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 12:12'))
+      trip1 = Trip.create(name: 'mytrip1', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 12:12'))
+      trip2 = Trip.create(name: 'mytrip2', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 12:12'))
       visit trips_path
 
       expect(page).to have_content('All Trips')
@@ -12,8 +12,8 @@ describe 'a visitor' do
       expect(page).to have_content(trip2.name)
     end
     it 'can click the name of the trip and see the trip show page' do
-      trip1 = Trip.create!(name: 'mytrip1', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 20:12'))
-      trip2 = Trip.create!(name: 'mytrip2', start_date: DateTime.parse('12/20/2015 12:12'), end_date: DateTime.parse('12/20/2015 12:12'))
+      trip1 = Trip.create(name: 'mytrip1', start_date: DateTime.parse('12/12/2015 12:12'), end_date: DateTime.parse('12/12/2015 20:12'))
+      trip2 = Trip.create!(name: 'mytrip2', start_date: DateTime.parse('12/12/2016 12:12'), end_date: DateTime.parse('12/12/2016 20:12'))
       visit trips_path
 
       click_on trip1.name
